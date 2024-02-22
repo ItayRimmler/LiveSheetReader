4135377975331a00e01e02c554782742d5fcd0b0
ItayRimmler <itay.rimmler@gmail.com>
2024-02-22 21:32:12 +0200

    Updated g.py

--- src/lib/all/g.py
+++ src/lib/all/g.py
@@ -1,4 +1,40 @@
-
-
 import numpy as np
 
+def cat(a, b):
+    a.append(npfy(b))
+    return a
+
+
+def npfy(a):
+    return np.array(a)
+
+def nothing(a):
+    return a
+
+def gray(image):
+    from cv2 import (cvtColor as color, COLOR_BGR2GRAY as gray)
+    return color(image, gray)
+
+def multicat(my_range, catter, catted, function=nothing):
+    for i in range(my_range):
+        catter = cat(catter, function(catted[i]))
+    return npfy(catter)
+
+
+def f(x, function):
+    return function(x)
+
+
+
+def length(end, beginning):
+    return end - beginning
+
+def significantly_greater(old, new, relativity):
+    if new - old > relativity:
+        return new
+    return old
+
+def about_the_same(a, b, relativity):
+    if np.abs(a - b) <= relativity:
+        return True
+    return False
