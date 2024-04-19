67f26076f7e9ebd43128ae750f60f37933d54a3a
ItayRimmler <itay.rimmler@gmail.com>
2024-04-19 16:01:29 +0300

    Itay commit 2: decided to work on a dir named ver2. instead of using a different branch where i could mistakenly merge wrongly and where its less comfortable to switch between the messy code i had and the new one, i just made a new directory, and ill build my code in it.

---
+++ src/lib/all/State.py
@@ -0,0 +1,22 @@
+# Starting with importing the General functions and the libraries
+from ver2.src.lib.all import g
+from ver2.src.lib.all import constants as c
+
+
+class State:
+    def __init__(self):
+        self.current = "Start"
+        self.log = []
+
+    def read(self):
+        return self.current
+
+    def get_log(self):
+        return self.log
+
+    def write(self, other):
+        self.log.append(self.current)
+        self.current = other
+
+    def show(self):
+        print(self.current)
