2c618f58ec4ed061d0c036ff605b96d1ddd24b9e
ItayRimmler <itay.rimmler@gmail.com>
2024-04-24 18:31:13 +0300

    Itay commit 4: Chain process is finally implemented and tested (in other platforms, like an online cpp IDE) in C++!

--- src/lib/chain_process/Chain.cpp
+++ src/lib/chain_process/Chain.cpp
@@ -1,34 +1,101 @@
-#include "Chain.h"
-#include <math>
+#include "C:/Users/User/PycharmProjects/LiveSheetReader/ver2/src/lib/chain_process/Chain.h"
+#include <cmath>
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <iostream>
 
-vector<int> Chain::get_groups() const{
+using namespace std;
+
+
+vector<int> Chain::get_groups() const {
     vector<int> values;
-    for (const auto& element : val) {
+    for (const auto& element : this->val) {
         values.push_back(element.get_group());
     }
     return values;
 }
 
-vector<int> Chain::get_indices() const{
+vector<int> Chain::get_indices() const {
     vector<int> values;
-    for (const auto& element : val) {
+    for (const auto& element : this->val) {
         values.push_back(element.get_index());
     }
     return values;
 }
 
 
-vector<string> Chain::get_types() const{
+vector<string> Chain::get_types() const {
     vector<string> values;
-    for (const auto& element : val) {
+    for (const auto& element : this->val) {
         values.push_back(element.get_type());
     }
     return values;
 }
 
-void Chain::calc_cont() const{
+void Chain::calc_cont() {
+    this->cont.clear();
+    for (int i = 0; i < this->get_len() - 1; i++) {
+        if (this->val[i + 1].get_index() - this->val[i].get_index() == 1) {
+            this->cont.push_back(1);
+        }
+        else { this->cont.push_back(0); }
+    }
+}
 
+void Chain::calc_score() {
+    int j = 1;
+    this->score = 0;
+    for (int i = 0; i < this->get_len() - 1; i++) {
+        if (this->cont[i] == 1) {
+            j++;
+            if (i == this->get_len() - 2) {
+                this->score = this->score + pow(j, j);
+                j = 1;
+            }
+        }
+        else {
+            this->score += pow(j, j);
+            j = 1;
+        }
+    }
+    this->score = this->score * pow(this->get_len(), this->get_len());
+}
+
+vector<Chain> Chain::possible_matches(const vector<Note>& other) const {
+    vector<Chain> list_of_possibilities;
+    int i = 0;
+    while (i < static_cast<int>(other.size())) {
+        int j = 0, counter = this->get_len();
+        vector<Note> possibility;
+        while ((counter > 0) && ((i + j) < this->get_len()) && (j < other.size())) {
+            if (other[j].get_type() == this->val[i + j].get_type()) {
+                possibility.push_back(this->val[i + j]);
+                counter--;
+            }
+            j++;
+        }
+        Chain temp(possibility);
+        list_of_possibilities.push_back(temp);
+        i++;
+    }
+    return list_of_possibilities;
+}
+
+vector<Note> Chain::match(const vector<Note>& other) const {
+    cout << 1 << endl;
+    vector<Chain> list_of_possibilities = possible_matches(other);
+    int highest_score = 0;
+    vector<Note> result;
+    for (auto& chain : list_of_possibilities) {
+        chain.calc_cont();
+        chain.calc_score();
+        int temp = chain.get_score();
+        if (temp > highest_score) {
+            result = chain.get_val();
+            highest_score = temp;
+        }
+    }
+    return result;
 }
 
--- src/lib/chain_process/Chain.h
+++ src/lib/chain_process/Chain.h
@@ -1,6 +1,7 @@
+#pragma once
 #include <vector>
 #include <string>
-#include "../all/Note.h"
+#include "C:/Users/User/PycharmProjects/LiveSheetReader/ver2/src/lib/all/Note.h"
 using namespace std;
 
 class Chain{
@@ -11,27 +12,31 @@ class Chain{
         double score;
         int page_num;
         //Private methods:
+        vector<Chain> possible_matches(const vector<Note>& other) const;
     public:
         //Getters:
-        vector<Note> get_val() const {return val;};
-        vector<int> get_cont() const {return cont;};
-        double get_score() const {return score;};
-        int get_page_num() const {return page_num;};
+        vector<Note> get_val() const {return this->val;};
+        vector<int> get_cont() const {return this->cont;};
+        double get_score() const {return this->score;};
+        int get_page_num() const {return this->page_num;};
+        int get_len() const {return static_cast<int>(this->val.size());};
         //Setters:
-        void set_val(const vector<Note>& other) {val = other;};
-        void set_score(const double& other) {score = other;};
-        void set_page_num(const int& other) {page_num = other;};
+        void set_val(const vector<Note>& other) {this->val = other;};
+        void set_score(const double& other) {this->score = other;};
+        void set_page_num(const int& other) {this->page_num = other;};
         //Constructors:
-        Chain(){val.push_back(Note()); cont = calc_cont(); score = calc_score(); page_num = 0;};
-        Chain(const vector<Note>& notes) : val(notes) {cont = calc_cont(); score = calc_score(); page_num = 0;};
-        Chain(const vector<Note>& notes, const int& pageNum) : val(notes), page_num(pageNum) {cont = calc_cont(); score = calc_score();};
-        Chain(const int& pageNum) : page_num(pageNum) {val.push_back(Note()); cont = calc_cont(); score = calc_score();};
+        Chain(){val.push_back(Note()); this->calc_cont(); this->calc_score(); this->page_num = 1;};
+        Chain(const Chain& other) {this->val = other.get_val(); this->page_num = other.get_page_num(); this->calc_cont(); this->calc_score();};
+        Chain(const vector<Note>& notes) : val(notes) {this->calc_cont(); this->calc_score(); this->page_num = 1;};
+        Chain(const vector<Note>& notes, const int& pageNum) : val(notes), page_num(pageNum) {this->calc_cont(); this->calc_score();};
+        Chain(const int& pageNum) : page_num(pageNum) {this->val.push_back(Note()); this->calc_cont(); this->calc_score();};
         //Operators:
-        Chain operator+()
+        Chain operator+(const vector<Note>& other) {this->val.insert(this->val.end(), other.begin(), other.end()); return Chain(this->val);};
         //Methods:
         vector<int> get_groups() const;
         vector<int> get_indices() const;
         vector<string> get_types() const;
-        void calc_score() const;
-        void calc_cont() const;
-}
+        void calc_score();
+        void calc_cont();
+        vector<Note> match(const vector<Note>& other) const;
+};
