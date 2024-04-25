5a069b43bd81d267a807d3169df27555062502b3
ItayRimmler <itay.rimmler@gmail.com>
2024-04-25 11:44:00 +0300

    Itay commit 7: Updated more docs. I believe that ver2/src/app/all and ver2/src/lib/all is tidy.

--- src/lib/audio_process/audio_process.py
+++ src/lib/audio_process/audio_process.py
@@ -5,7 +5,7 @@ from ver2.src.lib.all import constants as c
 def process_tempo(tempo):
     return 60/tempo
 
-def get_input(my_sound, name, frequency):
+def get_input(my_sound):
     my_sound.record()
     my_sound.send_to_cpp()
     #my_sound.note_detect(frequency, name)
